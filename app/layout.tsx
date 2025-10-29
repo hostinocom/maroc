3404886574f9fa31effcf52370abebc7d6661e3a
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-10-29 21:12:11 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -152,14 +152,12 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <noscript>
           <iframe
             src={`https://www.googletagmanager.com/ns.html?id=GTM-5QLCSWWS`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
-        </noscript>
 
         <Header />
         {children}
