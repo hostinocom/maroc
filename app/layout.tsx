c9f5ffa0b41c8bdd8405415311a8971a97c814d0
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-10-29 21:08:34 +0100

    add GTM by id writed

--- app/layout.tsx
+++ app/layout.tsx
@@ -138,26 +138,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr-MA">
+      {/* <!-- Google Tag Manager --> */}
       <Script id="gtm-script" strategy="afterInteractive">
         {`
-    (function(w,d,s,l,i){
-      w[l]=w[l]||[];
-      w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),
-          dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;
-      j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-      f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5QLCSWWS')
   `}
       </Script>
+      {/*  <!-- End Google Tag Manager --> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5QLCSWWS"
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-5QLCSWWS`}
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
