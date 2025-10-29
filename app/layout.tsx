c879a9aecb1cf9388837e206181b96d97157099b
pressinghichamapp-maker <pressinghichamapp@gmail.com>
2025-10-29 21:20:12 +0100

    updated script googleTagManager

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,31 +24,40 @@ export default function RootLayout({
   return (
     <html lang="fr-MA">
       {/* <!-- Google Tag Manager --> */}
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5QLCSWWS')
-  `}
-      </Script>
+
       {/*  <!-- End Google Tag Manager --> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-5QLCSWWS`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+        <iframe
+          src={`https://www.googletagmanager.com/ns.html?id=GTM-5QLCSWWS`}
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
 
         <Header />
         {children}
         <CookieBanner />
         <Footer />
-        
+
+        <script
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: 
+            `(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-5QLCSWWS")`
+          ,
+          }}
+        />
       </body>
     </html>
   );
